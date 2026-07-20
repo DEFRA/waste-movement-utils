284ab14d9ea325de44abbb546ad67a1acffe11fb
David Shepherd <6839214+shepda@users.noreply.github.com>
2026-07-20 14:47:59 +0100

    DWTA-250 Add basic auth plugin logging (#15)
    Added with console.log statements as this is hopefully temporary and it means we don't need to also deploy the Admin UI service to pass through the logger

--- src/helpers/validate-credentials.js
+++ src/helpers/validate-credentials.js
@@ -6,12 +6,24 @@
  * @returns {Function} Function to validate the user supplied credentials
  */
 export function validateCredentials(validCredentials) {
+  console.log('Basic Auth Plugin: Validating Credentials Started')
+
   validCredentials = validCredentials || []
 
   return async (_request, username, password) => {
+    console.log('Basic Auth Plugin: Checking Credentials')
+
     const base64EncodedCredentials = btoa(`${username}=${password}`)
-    const matchingCredential = validCredentials.find(
-      (cred) => cred === base64EncodedCredentials
+    const matchingCredential = validCredentials.find((cred) => {
+      console.log(
+        `Basic Auth Plugin: Found Credential To Check (isValid: ${cred === base64EncodedCredentials})`
+      )
+
+      return cred === base64EncodedCredentials
+    })
+
+    console.log(
+      `Basic Auth Plugin: Validating Credentials Finished (data: ${JSON.stringify({ isValid: !!matchingCredential, credentials: { username } })})`
     )
 
     return { isValid: !!matchingCredential, credentials: { username } }
