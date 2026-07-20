3356552fcb5f532d3aa68f3713b82c660b5db84f
David Shepherd <6839214+shepda@users.noreply.github.com>
2026-07-20 17:07:35 +0100

    DWTA-250 Update basic auth logging (#16)

--- src/plugins/basic-auth.js
+++ src/plugins/basic-auth.js
@@ -5,24 +5,31 @@ import { validateCredentials } from '../helpers/validate-credentials.js'
  * Sets up a plugin to validate the user supplied credentials with the valid credentials
  *
  * @param {String} validCredentials - The valid credentials
+ * @param {Object} logger - The logger
  *
  * @returns {Object} The auth plugin
  */
-export const basicAuth = (validCredentials) => ({
+export const basicAuth = (validCredentials, logger) => ({
   plugin: {
     name: 'basic-auth',
     async register(server) {
-      console.log('Basic Auth Plugin: Loaded Plugin')
+      if (logger) {
+        logger.debug('Basic Auth Plugin: Loaded Plugin')
+      }
 
       await server.register(Basic)
 
-      console.log('Basic Auth Plugin: Registered Plugin')
+      if (logger) {
+        logger.debug('Basic Auth Plugin: Registered Plugin')
+      }
 
       server.auth.strategy('basic', 'basic', {
         validate: validateCredentials(validCredentials)
       })
 
-      console.log('Basic Auth Plugin: Credential Validation Complete')
+      if (logger) {
+        logger.debug('Basic Auth Plugin: Credential Validation Complete')
+      }
     }
   }
 })
