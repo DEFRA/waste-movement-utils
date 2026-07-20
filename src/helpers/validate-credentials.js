2c5d2e97fc5100cfeb62d28cbb8eeb11dc1ebcb5
David Shepherd <david@x-designs.co.uk>
2026-07-20 15:31:21 +0100

    DWTA-250 Update basic auth logging

--- src/helpers/validate-credentials.js
+++ src/helpers/validate-credentials.js
@@ -2,29 +2,38 @@
  * Validates the user supplied credentials with the valid credentials
  *
  * @param {String} validCredentials - The valid credentials
+ * @param {Object} logger - The logger
  *
  * @returns {Function} Function to validate the user supplied credentials
  */
-export function validateCredentials(validCredentials) {
-  console.log('Basic Auth Plugin: Validating Credentials Started')
+export function validateCredentials(validCredentials, logger) {
+  if (logger) {
+    logger.debug('Basic Auth Plugin: Validating Credentials Started')
+  }
 
   validCredentials = validCredentials || []
 
   return async (_request, username, password) => {
-    console.log('Basic Auth Plugin: Checking Credentials')
+    if (logger) {
+      logger.debug('Basic Auth Plugin: Checking Credentials')
+    }
 
     const base64EncodedCredentials = btoa(`${username}=${password}`)
     const matchingCredential = validCredentials.find((cred) => {
-      console.log(
-        `Basic Auth Plugin: Found Credential To Check (isValid: ${cred === base64EncodedCredentials})`
-      )
+      if (logger) {
+        logger.debug(
+          `Basic Auth Plugin: Found Credential To Check (isValid: ${cred === base64EncodedCredentials})`
+        )
+      }
 
       return cred === base64EncodedCredentials
     })
 
-    console.log(
-      `Basic Auth Plugin: Validating Credentials Finished (data: ${JSON.stringify({ isValid: !!matchingCredential, credentials: { username } })})`
-    )
+    if (logger) {
+      logger.debug(
+        `Basic Auth Plugin: Validating Credentials Finished (data: ${JSON.stringify({ isValid: !!matchingCredential, credentials: { username } })})`
+      )
+    }
 
     return { isValid: !!matchingCredential, credentials: { username } }
   }
