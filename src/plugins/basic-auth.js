84c4b58bb1c88ebaad36a111d0622f73575a76e8
David Shepherd <david@x-designs.co.uk>
2026-07-20 14:07:50 +0100

    DWTA-250 Add basic auth plugin logging
    Added with console.log statements as this is hopefully temporary and it means we don't need to also deploy the Admin UI service to pass through the logger

--- src/plugins/basic-auth.js
+++ src/plugins/basic-auth.js
@@ -12,11 +12,17 @@ export const basicAuth = (validCredentials) => ({
   plugin: {
     name: 'basic-auth',
     async register(server) {
+      console.log('Basic Auth Plugin: Loaded Plugin')
+
       await server.register(Basic)
+
+      console.log('Basic Auth Plugin: Registered Plugin')
 
       server.auth.strategy('basic', 'basic', {
         validate: validateCredentials(validCredentials)
       })
+
+      console.log('Basic Auth Plugin: Credential Validation Complete')
     }
   }
 })
